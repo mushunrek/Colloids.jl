a908893392183b6c7bb4c136698d9e0b05460d87
mushunrek <kern@wias-berlin.de>
2023-10-15 10:44:35 +0200

    :sparkles: (points.jl) add conversion to PointList

--- src/points.jl
+++ src/points.jl
@@ -9,6 +9,16 @@ const Point = SVector{2, Float64}
 const PointList = Vector{Point}
 const PointMatrix = Matrix{Point}
 
+function PointList(m::Matrix{T}) where T <: Real 
+    size(m, 2) == 2 || error("Dimension mismatch") 
+    return copy(
+        reinterpret(
+            Point, 
+            vec(m)
+        )
+    )
+end
+
 sq_norm(x::Point) = x[1]^2 + x[2]^2
 dot(x::Point, y::Point) = x[1]*y[1] + x[2]*y[2]
 
