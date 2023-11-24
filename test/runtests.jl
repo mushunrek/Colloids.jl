0bec3f518d43e3f7f4ba4b59d3b5713b1ff92ebe
mushunrek <kern@wias-berlin.de>
2023-11-24 16:33:54 +0100

    :white_check_mark: update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,46 @@
 using Colloids
 using StaticArrays
 using Distributions
+#using BenchmarkTools
 using Test
 
+normal = Normal()
+
 @testset "Colloids.jl" begin
-    # Write your tests here.
+    initial = [ [1.42*i, 1.42*i] for i in 1:7 ]
+    #initial = [ [0.0, 0.0], [ 2.1,0.0 ], [1.0,0.93*2.1], [3.1 ,0.9*2.1] ]
+
+    # radius = 1.0, diffusivity = 1.0
+    colloid = Ball(1.0, 1.0, potential=Quadratic(strength=1.0))
+    # radius = 0.3, density = 30.0
+    fluid = Fluid(0.15, 30.0)
+
+    T = 1.0
+    Δt = 0.001
+    time_tolerance = 1e-8
+
+    sim = ColloidsInFluid(initial, colloid, fluid, T, Δt, time_tolerance)
+    #animate(sim, "test.gif", skipframes=10)
+    povray(sim, fps=30)
+    #to_csv(sim)
+
+    """colloid_initial = [ [1.42*i, 1.42*i] for i in 1:10 ]
+    semicolloid_initial = rand(normal, 10^3, 2)
+        
+    # radius = 1.0, diffusivity = 1.0
+    colloid = Ball(1.0, 1.0)
+    # radius = 0.2, diffusivity = 10.0
+    semicolloid = Ball(0.2, 1.0)
+        
+    T = 1.0
+    Δt = 0.1
+    time_tolerance = 1e-5
+        
+    @btime ColloidsInSemicolloids(colloid_initial, semicolloid_initial, colloid, semicolloid, T, Δt, time_tolerance)
+    #animate(sim, "test2.gif")"""
 end
 
-@testset "helper.jl" begin
+@test_skip @testset "helper.jl" begin
     include("../src/helper.jl")
     import .Helper
 
@@ -41,12 +74,11 @@ end
     # still need to test check_collision!
 end
 
-@testset "large_balls.jl" begin
+@test_skip @testset "large_balls.jl" begin
     include("../src/large_balls.jl")
 
     import .LargeBalls
 
-    normal = Normal()
     displacement = zeros(SVector{2, Float64}, 2)
     coords = copy(reinterpret(
         SVector{2, Float64}, 
@@ -70,6 +102,14 @@ end
                             ]
                         )
 
+    displacement = zeros(SVector{2, Float64}, 10^3)
+    coords = copy(reinterpret(
+        SVector{2, Float64},
+        rand(Normal(), 2*10^3)
+    ))
+    @benchmark LargeBalls.update_displacement!($displacement, $coords, $Σ, $rl, $rs, $zs, $Δt)
+    @benchmark LargeBalls.update_displacement2!($displacement, $coords, $Σ, $rl, $rs, $zs, $Δt)
+
     
     initial = copy(reinterpret(SVector{2, Float64}, [ 1.42*i for i in -1:10 ]))
     R = 1.0
@@ -82,5 +122,27 @@ end
     LargeBalls.save_colloids_to_pov(sim, LargeBalls.generate_foldername(sim))
     LargeBalls.animate(sim)
     
+end
+
+
+@test_skip @testset "all_balls.jl" begin
+    include("../src/all_balls.jl")
+    include("../src/helper.jl")
+
+    using .Helper 
+    using .AllBalls
+
+    colloid_initial = copy(reinterpret(SVector{2, Float64}, [ 1.0*i for i in -9:10 ]))
+    semicolloid_initial = copy(reinterpret(SVector{2, Float64}, 10.0 .* rand(normal, 2*10^4)))
+
+    colloid = Ball(1.0, 1.0, AllBalls.no_potential)
+    semicolloid = Ball(0.2, 1.0, x -> delayed_quadratic(x, delay=20.0, strength=0.01))
+
+    T = 1.0
+    Δt = 0.001
+    time_tolerance = 1e-10
+
+    sim = MixtureSimulation(colloid_initial, semicolloid_initial, colloid, semicolloid, T, Δt, time_tolerance)
+    animate(sim, "./test.gif", fps=5)
 end
 
